c0a77956c5fc04d9273b9eb54702813e60b2581d
admin <admin@roelt.local>
2023-03-20 11:32:36 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM php:8.1-fpm-alpine
+
+RUN apk update
+RUN apk add bash
+RUN apk add curl
+
+#Install composer
+
+RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+RUN mkdir "hello-world"
+
+RUN composer create-project laravel/laravel hello-world
+
+WORKDIR ./hello-world
+
+COPY ./routes/web.php ./routes
+COPY ./views/hello.blade.php ./resources/views
+
+EXPOSE 80/tcp
+
+ENTRYPOINT ["php", "artisan", "serve", "--host=0.0.0.0", "--port=8080"]
---
+++ resources/routes/web.php
@@ -0,0 +1,23 @@
+b97940c873ea:/var/www/html/hello-world# cat routes/web.php
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('hello', function(){
+    return view('hello');
+});
